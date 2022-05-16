d19cda7a2606ee4c28732eb18d489ccdd9c63c2e
Andrew Almeida <am.almeida000@gmail.com>
2022-05-16 19:55:24 +0000

    Completed javascript-operators-and-expressions exercise

---
+++ javascript-operators-and-expressions/main.js
@@ -0,0 +1,53 @@
+var width = 11;
+var height = 34;
+
+var area = width * height;
+
+console.log('Value of area: ', area);
+console.log('typeof area variable: ', typeof area);
+
+var bill = 169;
+var payment = 180;
+
+var change = payment - bill;
+
+console.log('Value of change: ', change);
+console.log('typeof change variable: ', typeof change);
+
+var quizzes = 85;
+var midterm = 88;
+var final = 97;
+
+var grade = (quizzes + midterm + final) / 3;
+
+console.log('Value of grade: ', grade);
+console.log('typeof grade variable: ', typeof grade);
+
+var firstName = 'Andrew';
+var lastName = 'Almeida';
+
+var fullName = firstName + ' ' + lastName;
+
+console.log('Value of fullName: ', fullName);
+console.log('typeof fullName variable: ', typeof fullName);
+
+var pH = 3;
+
+var isAcidic = pH < 7;
+
+console.log('Value of isAcidic: ', isAcidic);
+console.log('typeof isAcidic variable: ', typeof isAcidic);
+
+var headCount = 13;
+
+var isSparta = headCount === 300;
+
+console.log('Value of isSparta: ', isSparta);
+console.log('typeof isSparta variable: ', typeof isSparta);
+
+var motto = fullName;
+
+motto = fullName + ' is the GOAT';
+
+console.log('Value of motto: ', motto);
+console.log('typeof motto variable: ', typeof motto);

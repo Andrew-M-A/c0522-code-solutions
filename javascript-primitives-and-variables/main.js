337e53a6e273c186ec95e52613c6df4abf3109e0
Andrew Almeida <am.almeida000@gmail.com>
2022-05-16 17:31:04 +0000

    Completed javascript-primitives-and-variables exercise

---
+++ javascript-primitives-and-variables/main.js
@@ -0,0 +1,21 @@
+var fullName = 'Andrew Almeida';
+var isCool = false;
+var totalPets = 3;
+
+console.log('Value of full name: ', fullName);
+console.log('Value of isCool: ', isCool);
+console.log('Value of totalPets: ', totalPets);
+
+console.log('fullName type:', typeof fullName);
+console.log('isCool type:', typeof isCool);
+console.log('totalPets type: ', typeof totalPets);
+
+var unknown = null;
+
+console.log('Value of unknown: ', unknown);
+console.log('unknown type: ', typeof unknown);
+
+var never;
+
+console.log('Value of never: ', never);
+console.log('never type: ', never);

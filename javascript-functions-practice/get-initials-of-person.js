dbdb5be4f85e9bf00315debc9b415760f7d28c55
Andrew Almeida <am.almeida000@gmail.com>
2022-05-18 05:20:03 +0000

    Completed javascript-functions-practice

---
+++ javascript-functions-practice/get-initials-of-person.js
@@ -0,0 +1,7 @@
+/* exported getInitialsOfPerson */
+
+function getInitialsOfPerson(person) {
+  var firstName = person.firstName;
+  var lastName = person.lastName;
+  return firstName[0] + lastName[0];
+}

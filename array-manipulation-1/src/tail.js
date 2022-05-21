d1a2c780bb3ef42d1abc0cf51d9c763980ddc7c1
Andrew Almeida <am.almeida000@gmail.com>
2022-05-21 00:03:55 +0000

    in progress

--- array-manipulation-1/src/tail.js
+++ array-manipulation-1/src/tail.js
@@ -7,3 +7,12 @@ function tail(array) {
   }
   return tailEnd;
 }
+
+/*
+
+1.) create a new array literal
+2.) create a for loop that iterates through the length of our parameter (array)
+3.) since we're returning the values of the array AFTER the first index, push the value of the array at [i + 1]
+4.) return the new array
+
+*/

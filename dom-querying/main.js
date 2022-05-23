411f45da224ef90d1eb6f1adf09fa733b677d5ff
Andrew Almeida <am.almeida000@gmail.com>
2022-05-23 18:03:23 +0000

    revised requested changes

--- dom-querying/main.js
+++ dom-querying/main.js
@@ -1,13 +1,13 @@
 console.log('hello, world!');
 
 console.log(document.querySelector('h1'));
-console.dir('h1');
+console.dir(document.querySelector('h1'));
 
 console.log(document.querySelector('#explanation'));
-console.dir('#explanation');
+console.dir(document.querySelector('#explanation'));
 
 console.log(document.querySelector('.hint'));
-console.dir('.class');
+console.dir(document.querySelector('.hint'));
 
 console.log(document.querySelectorAll('p'));
 

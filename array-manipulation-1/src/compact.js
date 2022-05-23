f5432c2caa60ee173d3b8189ec5a80bdb1ffb946
Andrew Almeida <am.almeida000@gmail.com>
2022-05-23 05:38:18 +0000

    Completed array-manipulation-1 exercise

--- array-manipulation-1/src/compact.js
+++ array-manipulation-1/src/compact.js
@@ -10,3 +10,11 @@ function compact(array) {
   }
   return trueArray;
 }
+
+/* 1.) assign an empty array to a variable
+2.) use a for loop with the condition that,
+the initialization variable is less than the parameter.length
+increment the initialization variable by 1
+3.) use an if statement with the conditon (array[i]),
+call the push method of the empty array variable with an argument of array[i]
+4.) return the empty array */
--- array-manipulation-1/src/reverse.js
+++ array-manipulation-1/src/reverse.js
@@ -7,3 +7,9 @@ function reverse(array) {
   }
   return backwards;
 }
+
+/* 1.) assign an empty array to a variable
+2.) use a for loop with an initialization of i = array.length,
+a conditon of i >= 0, and then decrement i
+3.) call the push method of the empty array variable with an arguemtn of array[i]
+4.) return the empty array variable */

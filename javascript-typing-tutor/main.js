8946285a334edf1dec5a1817dbf50758fd14abda
Andrew Almeida <am.almeida000@gmail.com>
2022-05-25 00:58:13 +0000

    in progress

---
+++ javascript-typing-tutor/main.js
@@ -0,0 +1,13 @@
+// var $currentCharacter = document.querySelectorAll('span');
+
+function typeCheck(event) {
+
+/*  for (var i = 0; i <= $currentCharacter.length; i++) {
+    if ($currentCharacter[i].textContent === ) {
+      console.log('correct!');
+    }
+  }
+  */
+}
+
+document.addEventListener('keydown', typeCheck);

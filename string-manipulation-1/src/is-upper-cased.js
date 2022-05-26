85634de36afdfad4f06d2d5f00652480e519aa50
Andrew Almeida <am.almeida000@gmail.com>
2022-05-26 04:32:51 +0000

    working on pseudo code

--- string-manipulation-1/src/is-upper-cased.js
+++ string-manipulation-1/src/is-upper-cased.js
@@ -4,10 +4,12 @@ function isUpperCased(word) {
   if (word === word.toUpperCase(word)) {
     return true;
   }
-  return false;
+
 }
 
-/* 1.) use an if statement with the condition,
-  if the parameter is strictly equal to the parameter.toUpperCase(parameter),
-  return true
-  2.) else, return false */
+/*
+1.) define a function isUpperCased that takes one parameter, word, which is a JavaScript string
+2.) use an if statement to see if word is strictly equal to word upper-cased
+3.) if it is strictly equal, return true
+4.) if it is not, return false
+*/

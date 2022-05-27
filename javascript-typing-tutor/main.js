e7da2ea4aef64e60e6d307453cb1f40b36013437
Andrew Almeida <am.almeida000@gmail.com>
2022-05-27 07:16:28 +0000

    in progress

--- javascript-typing-tutor/main.js
+++ javascript-typing-tutor/main.js
@@ -1,13 +1,28 @@
-// var $currentCharacter = document.querySelectorAll('span');
+var $phrase = document.querySelectorAll('span');
+
+var counter = 0;
 
 function typeCheck(event) {
 
-/*  for (var i = 0; i <= $currentCharacter.length; i++) {
-    if ($currentCharacter[i].textContent === ) {
-      console.log('correct!');
-    }
+  if ($phrase[counter].textContent === event.key) {
+    $phrase[counter].className = '.green-span';
+    counter++;
   }
-  */
+
 }
 
 document.addEventListener('keydown', typeCheck);
+
+/* access span array $phrase
+if statement
+(get textContent at each index)
+
+if textContent value === event.key
+turn letter green
+and move underline to next index
+increment counter
+
+if textContent value !== event.key
+turn letter red
+keep underline in place + turn underline red
+don't increment counter */

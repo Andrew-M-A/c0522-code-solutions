ab1441835becab216f946be4aa6b52187feda577
Andrew Almeida <am.almeida000@gmail.com>
2022-05-28 03:48:09 +0000

    fixed pseudo code

--- object-manipulation-1/src/to-object.js
+++ object-manipulation-1/src/to-object.js
@@ -14,9 +14,9 @@ function toObject(keyValuePair) {
   containing two elements, a String key and any other JavaScript value
   2.) The function returns an Object with one property consisting of the key-value pair
   3.) define an empty object literal to hold the key-value pair
-  4.) get the first value of the key-value pair and assign that to a variable
-  5.) get the second value of the key-value pair and assign that to a variable
-  6.) use braket notation with the empty object and first variable of the key-value pair...
-      and assign the second value of the key-value pair to it
+  4.) get the first value of the parameter and assign that to a variable
+  5.) get the second value of the parameter and assign that to a variable
+  6.) use bracket notation with the empty object AT the variable from step 4...
+      and assign the variable from step 5 to it
   7.) return the result of that expression
 */

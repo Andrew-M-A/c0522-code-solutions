c4e2d2f76d98e98cb176decf1216c5b5b5a684b9
Andrew Almeida <am.almeida000@gmail.com>
2022-06-06 01:16:04 +0000

    in progress

--- string-manipulation-2/src/capitalize-words.js
+++ string-manipulation-2/src/capitalize-words.js
@@ -1,1 +1,9 @@
 /* exported capitalizeWords */
+
+function capitalizeWords(string) {
+  var words = string.split(' ');
+  for (var i = 0; i < words.length; i++) {
+    words[i] = words[i].charAt(0).toUpperCase() + words[i].slice(1).toLowerCase();
+  }
+  return words.join(' ');
+}
--- string-manipulation-2/src/ransom-case.js
+++ string-manipulation-2/src/ransom-case.js
@@ -1,1 +1,18 @@
 /* exported ransomCase */
+function ransomCase(string) {
+  var ransomWord = '';
+  for (var i = 0; i < string.length; i++) {
+    if (i % 2 === 0) {
+      ransomWord += string[i].toLowerCase();
+    } else {
+      ransomWord += string[i].toUpperCase();
+    }
+  }
+  return ransomWord;
+}
+
+/*
+1.) define a variable and assign it an empty string to hold the ransomCased string
+2.) use a for loop to loop through each letter
+3.)
+*/

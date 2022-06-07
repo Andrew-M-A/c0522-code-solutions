19265e0e8664fd706685e5c2690951f954f6d545
Andrew Almeida <am.almeida000@gmail.com>
2022-06-07 19:26:10 +0000

    in progress

--- string-manipulation-2/src/capitalize-word.js
+++ string-manipulation-2/src/capitalize-word.js
@@ -8,7 +8,7 @@ function capitalizeWord(word) {
 
 /*
 1.) check if the word parameter is strictly equal to 'javascript' lowercased
-2.) if it is, return 'JavaScript' with propert capitalization
+2.) if it is, return 'JavaScript' with proper capitalization
 3.) capitalize the first letter of the word parameter and add that to...
 4.) the rest of the word parameter lower cased
 5.) return the result of that expression
--- string-manipulation-2/src/capitalize-words.js
+++ string-manipulation-2/src/capitalize-words.js
@@ -7,3 +7,10 @@ function capitalizeWords(string) {
   }
   return words.join(' ');
 }
+
+/*
+1.) split each string up into single words
+2.) use a for loop to find the first letter of each word and capitalize it
+3.) lower case the rest of the word
+4.) rejoin the words
+*/

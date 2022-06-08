cc2989fa10aa46d4affa441f6abc3b996ee437e8
Andrew Almeida <am.almeida000@gmail.com>
2022-06-08 04:45:53 +0000

    completed javascript-bank exercise

--- javascript-bank/bank.js
+++ javascript-bank/bank.js
@@ -8,20 +8,17 @@ function Bank() {
 Bank.prototype.openAccount = function (holder, balance) {
   if (balance > 0 && Number.isInteger(balance) === true) {
     var account = new Account(this.nextAccountNumber, holder);
-    account.balance = balance;
+    account.deposit(balance);
     this.accounts.push(account);
-
     this.nextAccountNumber++;
     return account.number;
-  } else {
-    return null;
   }
+  return null;
 };
 
 Bank.prototype.getAccount = function (number) {
   for (var i = 0; i < this.accounts.length; i++) {
     if (number === this.accounts[i].number) {
-
       return this.accounts[i];
     }
   }
@@ -31,7 +28,7 @@ Bank.prototype.getAccount = function (number) {
 Bank.prototype.getTotalAssets = function () {
   var elTotalGran = 0;
   for (var i = 0; i < this.accounts.length; i++) {
-    elTotalGran += this.accounts[i].balance;
+    elTotalGran += this.accounts[i].getBalance();
   }
   return elTotalGran;
 };

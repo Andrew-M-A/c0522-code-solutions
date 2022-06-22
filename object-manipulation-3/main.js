f5d18885e4eb30cb36659c1de90df714b9b3e5fc
Andrew Almeida <am.almeida000@gmail.com>
2022-06-22 06:39:54 +0000

    in progress

---
+++ object-manipulation-3/main.js
@@ -0,0 +1,12 @@
+console.log('Lodash is loaded:', typeof _ !== 'undefined');
+
+var aj = [];
+var anthony = [];
+var andrew = [];
+var alex = [];
+
+var deck = [];
+
+var suits = ['Spades', 'Hearts', 'Diamonds', 'Clubs'];
+
+var rank = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A'];

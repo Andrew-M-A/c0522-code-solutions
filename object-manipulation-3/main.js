cb739f90191c923acb2bf26b700890add649d2a9
Andrew Almeida <am.almeida000@gmail.com>
2022-06-23 06:25:48 +0000

    got cards into deck... working on shuffling

--- object-manipulation-3/main.js
+++ object-manipulation-3/main.js
@@ -1,5 +1,14 @@
 console.log('Lodash is loaded:', typeof _ !== 'undefined');
 
+// create an array for each player
+// create an array for the deck
+// create an array of suits
+// create an arry of ranks
+// loop through the suits array and the ranks array at the same time
+// on each loop, create a card with properties with values of each suit/rank pair
+// should have 52 objects after
+// push those cards into the deck array
+
 var aj = [];
 var anthony = [];
 var andrew = [];
@@ -13,6 +22,7 @@ var ranks = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A'];
 
 for (var suit = 0; suit < suits.length; suit++) {
   for (var rank = 0; rank < ranks.length; rank++) {
-    console.log(rank);
+    var card = { suit: suits[suit], rank: ranks[rank] };
+    deck.push(card);
   }
 }

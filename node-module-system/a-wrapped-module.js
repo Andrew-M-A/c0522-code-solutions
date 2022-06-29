c5daf2950eb67c961a1646e7b1d507cb3279ffc7
Andrew Almeida <am.almeida000@gmail.com>
2022-06-29 17:40:23 +0000

    Completednode-module-system exercise

---
+++ node-module-system/a-wrapped-module.js
@@ -0,0 +1,11 @@
+console.log('Type of exports: ', typeof exports);
+console.log('Type of required: ', typeof require);
+console.log('Type of module:', typeof module);
+console.log('Type of __filename: ', typeof __filename);
+console.log('Type of __dirname', typeof __dirname);
+
+console.log('exports: ', exports);
+console.log('require: ', require);
+console.log('module: ', module);
+console.log('__filename: ', __filename);
+console.log('__dirname: ', __dirname);

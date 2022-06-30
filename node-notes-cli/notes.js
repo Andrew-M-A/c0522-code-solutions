bc5650fd34db89d67f4bd0beb6d0379723fa3818
Andrew Almeida <am.almeida000@gmail.com>
2022-06-30 22:58:25 +0000

    Completed node-notes-cli challenge

--- node-notes-cli/notes.js
+++ node-notes-cli/notes.js
@@ -2,20 +2,31 @@ const fs = require('fs');
 const jsonDerulo = require('./data.json');
 
 const command = process.argv[2];
+const action = process.argv[3];
+const updateText = process.argv[4];
 
 if (command === 'read') {
-  console.log(jsonDerulo.notes);
+  fs.readFile('data.json', 'utf8', (err, data) => {
+    if (err) throw err;
+    console.log(jsonDerulo.notes);
+  });
 }
 
 if (command === 'create') {
-  jsonDerulo.notes[jsonDerulo.nextId] = process.argv[3];
+  jsonDerulo.notes[jsonDerulo.nextId] = action;
   jsonDerulo.nextId++;
+}
+
+if (command === 'delete') {
+  delete jsonDerulo.notes[action];
+}
+
+if (command === 'update') {
+  jsonDerulo.notes[action] = updateText;
 }
 
 const updates = JSON.stringify(jsonDerulo, null, 2);
 
-fs.writeFile('data.json', updates, 'utf8', err => {
+fs.writeFile('data.json', updates + '\n', 'utf8', err => {
   if (err) throw err;
 });
-
-console.log(jsonDerulo);

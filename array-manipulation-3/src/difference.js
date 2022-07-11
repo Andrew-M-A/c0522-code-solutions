8c81cb0dfa07d91472b25c10677bfaac70a16b52
Andrew Almeida <am.almeida000@gmail.com>
2022-07-11 19:46:26 +0000

    in progress

--- array-manipulation-3/src/difference.js
+++ array-manipulation-3/src/difference.js
@@ -1,14 +1,12 @@
 /* exported difference */
 
+// define an empty array to hold the unique values
+// loop through the first array
+// within that loop, loop through the second array
+// check if the values within the first array are in the second
+// if they aren't, push into the empty array
+// return the array defined in step 1
+
 function difference(first, second) {
 
-  var asymmetry = [];
-  for (var i = 0; i < first.length; i++) {
-    for (var k = 0; k < second.length; k++) {
-      if (!second.includes(first[i])) {
-        asymmetry.push(first[i]);
-      }
-    }
-  }
-  return asymmetry;
 }

ab948aeae4beac987153dcbb641f628f9a17c8f7
Andrew M Almeida <am.almeida000@gmail.com>
2022-08-23 02:09:02 +0000

    Completed react-validated-input exercise

--- react-validated-input-component/src/validated-input.jsx
+++ react-validated-input-component/src/validated-input.jsx
@@ -11,7 +11,6 @@ export class ValidatedInput extends React.Component {
 
   handleInput(event) {
     this.setState({ password: event.target.value });
-
   }
 
   render() {
@@ -21,16 +20,29 @@ export class ValidatedInput extends React.Component {
     } else {
       icon = 'pass-red fa-solid fa-xmark fa-xl';
     }
+
+    let passText;
+    if (this.state.password.length === 0) {
+      passText = 'A password is required.';
+    } else if (this.state.password.length < 8) {
+      passText = 'Your password is too short';
+    } else {
+      passText = '';
+      icon = 'pass-green fa-solid fa-check fa-xl';
+    }
+
     return (
-     <div>
+     <div className='pass-div'>
        <form action="`">
        <div><label htmlFor="password"> Password</label></div>
         <input
+        className='input'
         name='password'
         onChange={this.handleInput}
-        type="text" />
+        type="password" />
           <i className={icon}></i>
        </form>
+        <span className='pass-text'>{passText}</span>
      </div>
     );
   }
